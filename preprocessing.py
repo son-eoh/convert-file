d31153e1c7ec15fc66b6936a1a27e8c9fcb5346e
Son Mac <son.lg@eoh.io>
2023-04-30 22:21:14 +0700

    done

--- preprocessing.py
+++ preprocessing.py
@@ -10,8 +10,9 @@ if '.doc' in filename:
     text = textract.process(filename)
     text = text.decode("utf-8")
     text = text.replace('|', '')
-    text = re.sub(r'\s+$', '', text, flags=re.MULTILINE)  # remove spacing from the right
-    text = re.sub(r"^\s{2}(.+)$", '', text, flags=re.MULTILINE)  # remove spacing from the right
+    text = re.sub(r'\s+$', '', text, flags=re.MULTILINE)  # remove spacing from right
+    text = re.sub(r' {2,}', ' ', text)  # replace all space > 2 with 1 space
+    text = "\n".join([line.lstrip() for line in text.split("\n")])  # remove spacing from left
 
 if '.docx' in filename:
     text = docx2txt.process(filename)
@@ -22,4 +23,4 @@ text = re.sub(pattern, "", text)
 with open(output, 'w') as f:
     f.write(text)
 
-print('Done')
+print(f'Done, check file: {output}')
