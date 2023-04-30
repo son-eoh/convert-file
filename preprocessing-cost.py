f95654953d6ced5cff79eea079abbd36deb7ec6f
Son Mac <son.lg@eoh.io>
2023-04-30 21:47:36 +0700

    save

---
+++ preprocessing-cost.py
@@ -0,0 +1,9 @@
+import aspose.words as aw
+
+doc = aw.Document("test.doc")
+
+doc.save("test-aw.txt")
+
+# python 1200$ / year
+# Can try convert, but half file, a lot file
+# Pricing: https://purchase.aspose.com/pricing/words/python-net
--- preprocessing.py
+++ preprocessing.py
@@ -5,11 +5,14 @@ import textract
 filename = 'test.doc'
 output = 'test.txt'
 
+# read doc file to plain text
+
+
 if '.doc' in filename:
-    text = textract.process(filename, method='tesseract', skip_tables=True)
+    text = textract.process(filename)
     text = text.decode("utf-8")
-    # text = text.replace('|', '')
-    text = re.sub(r'\s+$', '', text, flags=re.MULTILINE)
+    text = text.replace('|', '')
+    text = re.sub(r'\s+$', '', text, flags=re.MULTILINE)  # remove spacing from the right
 
 if '.docx' in filename:
     text = docx2txt.process(filename)
